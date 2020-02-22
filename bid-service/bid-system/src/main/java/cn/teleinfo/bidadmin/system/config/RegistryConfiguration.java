988942711c8a12b12824d46e68ce8a4ed12b2fc3
PengFei Shan <shanpengfei7@163.com>
2020-02-22 17:18:15 +0800

    字典接口对外开发功能完成

---
+++ bid-service/bid-system/src/main/java/cn/teleinfo/bidadmin/system/config/RegistryConfiguration.java
@@ -0,0 +1,16 @@
+package cn.teleinfo.bidadmin.system.config;
+
+import org.springblade.core.secure.registry.SecureRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RegistryConfiguration {
+
+    @Bean
+    public SecureRegistry secureRegistry() {
+        SecureRegistry secureRegistry = new SecureRegistry();
+        secureRegistry.excludePathPatterns("/front/**");
+        return secureRegistry;
+    }
+}
---
+++ bid-service/bid-system/src/main/java/cn/teleinfo/bidadmin/system/front/FrontDictController.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2018-2028, Chill Zhuang 庄骞 (smallchill@163.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.teleinfo.bidadmin.system.front;
+
+import cn.teleinfo.bidadmin.system.entity.Dict;
+import cn.teleinfo.bidadmin.system.service.IDictService;
+import cn.teleinfo.bidadmin.system.vo.DictVO;
+import cn.teleinfo.bidadmin.system.wrapper.DictWrapper;
+import io.swagger.annotations.*;
+import lombok.AllArgsConstructor;
+import org.springblade.core.boot.ctrl.BladeController;
+import org.springblade.core.mp.support.Condition;
+import org.springblade.core.tool.api.R;
+import org.springblade.core.tool.node.INode;
+import org.springblade.core.tool.utils.Func;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+import static cn.teleinfo.bidadmin.common.cache.CacheNames.DICT_LIST;
+import static cn.teleinfo.bidadmin.common.cache.CacheNames.DICT_VALUE;
+
+/**
+ * 控制器
+ *
+ * @author Chill
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping("/front/dict")
+@Api(value = "字典", tags = "对外字典接口")
+public class FrontDictController extends BladeController {
+
+	private IDictService dictService;
+
+	/**
+	 * 列表
+	 */
+	@GetMapping("/list")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "code", value = "字典编号", paramType = "query", dataType = "string"),
+		@ApiImplicitParam(name = "dictValue", value = "字典名称", paramType = "query", dataType = "string")
+	})
+	@ApiOperationSupport(order = 2)
+	@ApiOperation(value = "列表", notes = "传入dict")
+	public R<List<INode>> list(@ApiIgnore @RequestParam Map<String, Object> dict) {
+		@SuppressWarnings("unchecked")
+		List<Dict> list = dictService.list(Condition.getQueryWrapper(dict, Dict.class).lambda().orderByAsc(Dict::getSort));
+		return R.data(DictWrapper.build().listNodeVO(list));
+	}
+
+	/**
+	 * 获取字典树形结构
+	 *
+	 * @return
+	 */
+	@GetMapping("/tree")
+	@ApiOperationSupport(order = 3)
+	@ApiOperation(value = "树形结构", notes = "树形结构")
+	public R<List<DictVO>> tree() {
+		List<DictVO> tree = dictService.tree();
+		return R.data(tree);
+	}
+
+	/**
+	 * 获取字典
+	 *
+	 * @return
+	 */
+	@GetMapping("/dictionary")
+	@ApiOperationSupport(order = 6)
+	@ApiOperation(value = "获取字典", notes = "获取字典")
+	public R<List<Dict>> dictionary(String code) {
+		List<Dict> tree = dictService.getList(code);
+		return R.data(tree);
+	}
+
+
+}
