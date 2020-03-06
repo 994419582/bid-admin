039f092f9e745fb71b3f9f974f1ef032f4cb729a
PengFei Shan <shanpengfei7@163.com>
2020-03-06 12:05:16 +0800

    群管理员踢除用户

--- bid-service/bid-soybean/src/main/java/cn/teleinfo/bidadmin/soybean/wxfront/WxUserGroupController.java
+++ bid-service/bid-soybean/src/main/java/cn/teleinfo/bidadmin/soybean/wxfront/WxUserGroupController.java
@@ -15,23 +15,30 @@
  */
 package cn.teleinfo.bidadmin.soybean.wxfront;
 
+import cn.teleinfo.bidadmin.soybean.entity.Group;
 import cn.teleinfo.bidadmin.soybean.entity.UserGroup;
 import cn.teleinfo.bidadmin.soybean.service.IGroupLogService;
 import cn.teleinfo.bidadmin.soybean.service.IGroupService;
 import cn.teleinfo.bidadmin.soybean.service.IUserGroupService;
 import cn.teleinfo.bidadmin.soybean.vo.UserGroupVO;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.exceptions.ApiException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiOperationSupport;
 import lombok.AllArgsConstructor;
 import org.springblade.core.boot.ctrl.BladeController;
 import org.springblade.core.tool.api.R;
+import org.springblade.core.tool.utils.Func;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *  控制器
@@ -88,7 +95,51 @@ public class WxUserGroupController extends BladeController {
 	@ApiOperation(value = "群管理员踢除用户", notes = "传入用户ID，群组ID，操作人ID")
 	public R save(@Valid @RequestBody UserGroupVO userGroup) {
 		try {
-			return R.status(userGroupService.managerRemoveUser(userGroup));
+
+			//校验改群及其子群下是否有此用户
+			List<Integer> groupUserIds = groupService.selectUserIdByParentId(userGroup.getGroupId());
+			if (!groupUserIds.contains(userGroup.getManagerId())) {
+				throw new ApiException("改组织下未发现ID等于" + userGroup.getManagerId() + "的用户");
+			}
+			//获取用户是管理员的群
+			LambdaQueryWrapper<Group> queryWrapper = Wrappers.<Group>lambdaQuery().eq(Group::getStatus, Group.NORMAL);
+			List<Group> managerGroups = groupService.list(queryWrapper);
+			managerGroups.removeIf(group -> {
+				String managers = group.getManagers();
+				if (Func.toIntList(managers).contains(userGroup.getUserId())) {
+					return false;
+				}
+				Integer createUser = group.getCreateUser();
+				if (createUser != null && createUser.equals(userGroup.getUserId())) {
+					return false;
+				}
+				return true;
+			});
+			//获取用户管理的所有用户
+			ArrayList<Integer> managerUserIds = new ArrayList<>();
+			for (Group managerGroup : managerGroups) {
+				List<Integer> userIds = groupService.selectUserIdByParentId(managerGroup.getId());
+				managerUserIds.addAll(userIds);
+			}
+			//校验此用户是否有权限设置管理员
+			if (managerUserIds.contains(userGroup.getUserId())) {
+				// 删除
+				userGroup.setStatus(UserGroup.DELETE);
+				boolean flag = userGroupService.updateById(userGroup);
+				if (flag) {
+					Group group = groupService.getById(userGroup.getGroupId());
+					int count = group.getUserAccount();
+					count--;
+					group.setUserAccount(count);
+					return R.status(groupService.updateById(group));
+				} else {
+					return R.fail("群人数减1失败");
+				}
+			} else {
+				// 不能删除
+				return R.fail("没有删除权限");
+			}
+
 		} catch (Exception e) {
 			return R.fail(e.getMessage());
 		}
