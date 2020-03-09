5f936eae93edf6c5571e1bf046a99ea165f56da9
PengFei Shan <shanpengfei7@163.com>
2020-03-09 11:23:39 +0800

    nacos配置

--- bid-common/src/main/java/cn/teleinfo/bidadmin/common/constant/NacosConstant.java
+++ bid-common/src/main/java/cn/teleinfo/bidadmin/common/constant/NacosConstant.java
@@ -25,8 +25,9 @@ public interface NacosConstant {
     /**
      * nacos 地址
      */
-//    String NACOS_ADDR = "127.0.0.1:8848";
-    String NACOS_ADDR = "172.17.3.40:8888";
+    String NACOS_ADDR = "127.0.0.1:8848";   // 本地环境
+//    String NACOS_ADDR = "172.17.3.40:8888";   //  生产环境
+//    String NACOS_ADDR = "39.100.208.235:8848";  //    测试环境
 
     /**
      * nacos 配置前缀
